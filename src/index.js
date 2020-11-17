b1fcb5bc5c4a7740fd2c2483f13ec2f7dc6b4b5a
Дима Олейник <dimaoliynyk@MacBook-Air-Dima.local>
2020-11-17 18:07:28 +0200

    fx

--- src/index.js
+++ src/index.js
@@ -9,14 +9,12 @@ import {attention} from './js/error.js';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
-console.log(refs);
 
 // fetch(`https://restcountries.eu/rest/v2/all`).then(respone => respone.json()).then(data => console.log(data));
 refs.input.addEventListener('input', debounce((event)=>{
     let query = event.target.value;
-    console.log(query);
     fetchCountries(query).then(data=>{
-        console.log(data);
+        refs.ul.innerHTML = '';
         if(data.length >= 2 && data.length <= 10){
             insertElement(countryList, data, refs.ul);
         }else if(data.length === 1){
